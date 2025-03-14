4b7d0106189969126e3fbdda839db844bd2e4c33
DenisKurko <kurkodenis06@gmail.com>
2025-03-14 17:13:08 +0200

    Add Routes (React)

--- whattoread/frontend/src/components/app.js
+++ whattoread/frontend/src/components/app.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import { createRoot } from "react-dom/client";
+import Home from "./home";
 
 export default class App extends Component{
     constructor(props){
         super(props);
     }
+
     render() {
-        return <h1>TEST REACT TEST</h1>;
+        return (
+        <div>
+            <Home />
+        </div>
+        );
     }
 }
 
---
+++ whattoread/frontend/src/components/home.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { 
+    BrowserRouter as Router, 
+    Routes, 
+    Route, 
+    Link, 
+    Navigate
+} from "react-router-dom";
+
+import Search from "./search";
+import Profile from "./profile";
+
+export default class Home extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return(
+        <Router>
+            <Routes>
+                <Route path='/' element={<p>HOME PAGE</p>}/>
+                <Route path='/profile' element={<Profile/>}/>
+                <Route path='/search' element={<Search/>}/>
+            </Routes>
+        </Router>
+        );
+    }
+}
